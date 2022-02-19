87932ff107863500aa4f263789368e539df929f2
Luke <lukemacy1@gmail.com>
2022-02-18 19:52:48 -0700

    moved discord bot to its own repository

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # discord-notif-mining-bot
+python3 bot.py
+
+Environment Variables:
+- DISCORD_TOKEN (Discord API Token)
+- MINER (Wallet Address)
+- CHANNEL_ID (Discord Channel ID)
---
+++ unsplash_listener.py
@@ -0,0 +1,12 @@
+import requests
+
+def unsplash():
+  URL = 'https://source.unsplash.com/random'
+  res = 'Unsplash Error'
+
+  response_API = requests.get(URL)
+  if(str(response_API) == "<Response [200]>"):
+    res = response_API.url
+    print(res)
+
+  return res
